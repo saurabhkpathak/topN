d3071d3948ae9d50588c0545029ea74bbc1a0016
Saurabh Pathak <pathaks@tcd.ie>
2017-05-14 15:34:14 +0100

    setting angular project using yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name topNApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the topNApp
+ */
+angular.module('topNApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name topNApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the topNApp
+ */
+angular.module('topNApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
